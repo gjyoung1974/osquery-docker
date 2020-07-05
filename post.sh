77731238579d07889d46b3fcf7cf3615eb5bec98
Gordon Young <gjyoung1974@gmail.com>
2020-07-04 19:26:23 -0700

    Delete post.sh

--- post.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -v -X POST -T test.json https://endpoint6.collection.us2.sumologic.com/receiver/v1/http/ZaVnC4dhaV1_QO9hz06Eq-ZVOvtHUIKfML9GMUqbnCKcc_3G1Yp6h4N3SWiOKpTuuBZSTsADrO-j6gQjtDiszVEOpwzhkxPQcMgsZKGVKpYtx1ri4H6qQA==
-
